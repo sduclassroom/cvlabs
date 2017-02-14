bc450e5a040b9c8166d3c8b10d02584eb086c910
konsteacher <konstantin.latuta@sdu.edu.kz>
2017-02-14 12:36:20 +0600

    Update x3-3.cpp

--- x3-3/x3-3.cpp
+++ x3-3/x3-3.cpp
@@ -1,9 +1,7 @@
 /*x3-3. Compact matrix and vector template types:
-a. Using the cv::Mat<> and cv::Vec<> templates (respectively), create a 3 × 3
-matrix and 3-row vector.
+a. Using the cv::Mat<> and cv::Vec<> templates (respectively), create a 3 × 3 matrix and 3-row vector.
 b. Can you multiply them together directly? If not, why not?
-c. Try type-casting the vector object to a 3 × 1 matrix, using the cv::Mat<> tem‐
-plate. What happens now?
+c. Try type-casting the vector object to a 3 × 1 matrix, using the cv::Mat<> template. What happens now?
 */
 
 int main(){
