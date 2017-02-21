718cd32b4087c214af14075473041728c33ad242
Konstantin Latuta <konstantin.latuta@sdu.edu.kz>
2017-02-21 11:53:02 +0600

    add lab3

---
+++ lab3/x5-1/main.cpp
@@ -0,0 +1,20 @@
+/* x5-1 (stub for the exercise)
+
+This exercise will accustom you to the idea of many functions taking matrix
+types. Create a two-dimensional matrix with three channels of type byte with
+data size 100x100. Set all the values to 0.
+
+a. Draw a circle in the matrix using
+   void cv::circle(InputOutputArray img,cv::point center, intradius, cv::Scalar color, int thickness=1,int line_type=8, int shift=0) .
+
+b. Display this image using methods described in Chapter 2.
+
+*/
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argc, char*argv[]) {
+    // your code here
+    return 0;
+}
---
+++ lab3/x5-3/main.cpp
@@ -0,0 +1,11 @@
+/*x5-3
+Create a three-channel RGB image of size 100x100. Clear it.
+Use pointer arithmetic to draw a green square between (20, 5) and (40, 20).
+*/
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argc, char*argv[]) {
+    // code here
+    return 0;
+}
